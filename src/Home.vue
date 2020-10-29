b0d57e703dfd30918c8837a43ce33ad8af372d47
jhy <2264573476@qq.com>
2020-10-29 18:21:47 +0800

    initt

---
+++ src/Home.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="root-home">
+      Home
+      <div class="btn-container">
+        <UIButton @click="onClick"
+          :xsmall="xsmall"
+          :small="small"
+          :large="large"
+          :xlarge="xlarge"
+        >点我呀</UIButton>
+      </div>
+      <div class="btn-group">
+        <UIButton @click="resize('xsmall')">超小</UIButton>
+        <UIButton @click="resize('small')">小</UIButton>
+        <UIButton @click="resize('normal')">正常</UIButton>
+        <UIButton @click="resize('large')">大</UIButton>
+        <UIButton @click="resize('xlarge')">超大</UIButton>
+      </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue } from 'vue-property-decorator';
+import UIButton from '@/components/UIkit/UIButton.vue';
+
+@Component({
+  components: {
+    UIButton,
+  }
+})
+
+export default class App extends Vue {
+
+  private xsmall = false;
+  private small = false;
+  private large = false;
+  private xlarge = false;
+
+  private resize(name: string) {
+    switch (name) {
+      case 'xsmall':
+        this.xsmall = true;
+        this.small = false;
+        this.large = false;
+        this.xlarge = false;
+        break;
+      case 'small':
+        this.xsmall = false;
+        this.small = true;
+        this.large = false;
+        this.xlarge = false;
+        break;
+      case 'normal':
+        this.xsmall = false;
+        this.small = false;
+        this.large = false;
+        this.xlarge = false;
+        break;
+      case 'large':
+        this.xsmall = false;
+        this.small = false;
+        this.large = true;
+        this.xlarge = false;
+        break;
+      case 'xlarge':
+        this.xsmall = false;
+        this.small = false;
+        this.large = false;
+        this.xlarge = true;
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+  private onClick (e: MouseEvent) {
+    console.log(e)
+  }
+}
+
+
+</script>
+
+<style scoped lang="stylus">
+
+.btn-container    
+  width 480px
+  height 160px
+  // background-color red
+  display flex
+  justify-content center
+  align-items center
+</style>
---
+++ src/components/UIkit/UIButton.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+      <button
+        class="ui-btn"
+        :class="{
+          'ui-btn-xsmall': xsmall,
+          'ui-btn-small': small,
+          'ui-btn-large': large,
+          'ui-btn-xlarge': xlarge,
+        }"
+        @click="onClickBtn"
+      >
+        <slot>Button</slot>
+      </button>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue, Emit, Prop } from 'vue-property-decorator';
+
+@Component
+
+export default class App extends Vue {
+
+  @Prop(Boolean) private xsmall: boolean | undefined
+  @Prop(Boolean) private small: boolean | undefined
+  @Prop(Boolean) private large: boolean | undefined
+  @Prop(Boolean) private xlarge: boolean | undefined
+
+  @Emit('click') private emitClickEvent (e: MouseEvent) {
+    console.log(e)
+  }
+
+  private mounted() {
+    console.log(this.large)
+  }
+
+  private onClickBtn (e: MouseEvent) {
+    this.emitClickEvent(e);
+  }
+}
+
+
+</script>
+
+<style scoped lang="stylus">
+resize(minWidth, height, paddingLR, fontSize) {
+	min-width: minWidth,
+	height: height,
+	padding: 0 paddingLR,
+	font-size: fontSize,
+}
+.ui-btn {
+	min-width 64px
+	height 36px
+	padding 0 16px
+	border 0 solid black
+	border-radius 4px
+	font-size 0.875rem
+	font-weight 500
+	letter-spacing 0.09em
+	outline none
+	background-color #2D8CF0
+	cursor pointer
+	user-select none
+}
+.ui-btn-xsmall {
+	resize(36px, 20px, 9px, 0.625rem)
+}
+  
+.ui-btn-small {
+	resize(50px, 28px, 12px, 0.75rem)
+}
+  
+.ui-btn-large {
+	resize(78px, 44px, 19px, 0.875rem)
+}
+  
+.ui-btn-xlarge {
+	resize(92px, 52px, 23px, 1rem)
+}
+</style>
+
